e7d0c578255615190fc91347f4c76b0526dfc511
Sha Nia Siahaan <shaniashn76@gmail.com>
2025-08-24 23:58:14 +0700

    Refactor HomePage layout and capsule link
    Moved buttons inside the section element and replaced the 'Create new capsule' button with a router-link to '/capsule-form'. Added a 'status' data property and updated sign out log message. Commented out the mounted hook for user data fetching.

--- src/HomePage.vue
+++ src/HomePage.vue
@@ -1,8 +1,9 @@
 <template>
-  <section>Hi {{ fullname }}</section>
-  <button @click="signOut">Sign Out</button>
-
-  <button @click="goToCapsule">Create new capsule</button>
+  <section>
+    Hi {{ fullname }}
+    <button @click="signOut">Sign Out</button>
+    <router-link to="/capsule-form">Create new capsule</router-link>
+  </section>
 </template>
 
 <script>
@@ -14,6 +15,7 @@ export default {
   data() {
     return {
       fullname: '',
+      status: 'editing',
     }
   },
   methods: {
@@ -23,7 +25,7 @@ export default {
       if (error) {
         console.error('Error signing out: ', error)
       } else {
-        console.log('Signed out')
+        console.log('Signed out success')
       }
       // No need to manually redirect here - the global auth state listener in App.vue will handle it
     },
@@ -32,15 +34,15 @@ export default {
       this.$router.push('/home/capsule-form')
     },
   },
-  async mounted() {
-    const { data } = await supabase.auth.getUser()
+  // async mounted() {
+  //   const { data } = await supabase.auth.getUser()
 
-    if (data.user) {
-      // Set fullname from user metadata or email
-      this.fullname = data.user.user_metadata?.full_name || data.user.email
-    }
+  //   if (data.user) {
+  //     // Set fullname from user metadata or email
+  //     this.fullname = data.user.user_metadata?.full_name || data.user.email
+  //   }
 
-    console.log('getUser mounted homepage', data)
-  },
+  //   console.log('getUser mounted homepage', data)
+  // },
 }
 </script>
