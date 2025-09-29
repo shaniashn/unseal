ff09ae97ca2410bcf26e695fc4158e42042d1e51
Sha Nia Siahaan <shaniashn76@gmail.com>
2025-09-30 00:04:25 +0700

    Remove commented router import in App.vue
    Cleaned up App.vue by deleting an unused, commented-out import statement for the router. This improves code readability.

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,6 @@
 <script setup>
 import { ref, onMounted } from 'vue'
 import { supabase } from './lib/supabaseClient'
-// import router from './router/routers'
 import { useRouter } from 'vue-router'
 
 const session = ref()
